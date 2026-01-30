97d81b1c0613de47ece465dfce6f821472989914
ReinieriRicardo <rikyreinieri@gmail.com>
2026-01-29 21:10:47 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleDiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
